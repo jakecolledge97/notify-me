e7e7c3f61e2e03cd1e8e0cfca5c731a5e2e0bdec
Jake Colledge <jakecolledgework@gmail.com>
2021-11-17 23:03:59 +1030

    added app.get to get api info

--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,25 @@
 const express = require('express');
+//get api db
+const db = require('./db/db.json')
 
 const PORT = 3001;
 
 const app = express();
 
+//uses the public folder
 app.use(express.static('public'))
 
+//gets index page
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
-app.get('/notes.html', (req,res) => 
+//gets notes page
+app.get('/notes.html', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
-)
+
+})
+//gets api info
+app.get('/api/notes', (req,res) => res.json(db));
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
