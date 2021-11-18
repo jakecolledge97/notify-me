96b88651a392494121a30b334c1750457b06c723
Jake Colledge <jakecolledgework@gmail.com>
2021-11-19 04:24:38 +1030

    save notes button will now save notes

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -2,13 +2,16 @@ const notes = require('express').Router();
 const noteFile = require('../db/db.json')
 const fs = require('fs')
 const path = "../db/db.json"
+const util = require('util')
+const readFromFile = util.promisify(fs.readFile);
 
 notes.get('/', (req,res) => {
-    res.send(noteFile)
+    readFromFile(require('path').resolve(__dirname, '../db/db.json')).then((data) => res.json(JSON.parse(data)))
 })
 
 notes.post('/', (req, res) => {
     console.log(req.body);
+
     fs.readFile(require('path').resolve(__dirname, '../db/db.json'), 'utf8', (err,data) => {
         if(err){
             console.log(err)
@@ -17,9 +20,11 @@ notes.post('/', (req, res) => {
             const parsedData = JSON.parse(data);
             console.log(parsedData);
             parsedData.push(req.body)
-            fs.writeFile(require('path').resolve(__dirname, '../db/db.json'), JSON.stringify(parsedData, null, 4))
+            fs.writeFile(require('path').resolve(__dirname, '../db/db.json'), JSON.stringify(parsedData, null, 4), (err) => err ? console.log(err) : console.log(parsedData)
+            );
         }
     })
+    res.json(`New note successfully added!`)
 })
 
 module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -6,6 +6,9 @@ const api = require('./routes/index.js')
 const PORT = 3001;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api', api)
 //uses the public folder
 app.use(express.static('public'))
