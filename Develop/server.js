2ba1d6e25f9bfe8834a2642f22f888e60a8b1cfa
John Conway <88188872+jakecolledge97@users.noreply.github.com>
2021-11-18 22:15:06 +1030

    started changing routes

--- Develop/server.js
+++ Develop/server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 //get api db
 const db = require('./db/db.json')
+const api = require('./routes/notes')
 
 const PORT = 3001;
 
 const app = express();
-
+app.use('/api', api)
 //uses the public folder
 app.use(express.static('public'))
 
@@ -19,8 +20,6 @@ app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 
 })
-//gets api info from db.json
-app.get('/api/notes', (req,res) => res.json(db));
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
